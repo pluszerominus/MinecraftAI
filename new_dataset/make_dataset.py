00e5817cb2a043356521c8b3626ae33079d6042a
Bulaev Yaroslav <bulaev.yaroslav@yandex.ru>
2023-02-06 17:12:12 +0300

    removed old dataset and changed make script

--- new_dataset/make_dataset.py
+++ new_dataset/make_dataset.py
@@ -1,54 +1,89 @@
-from pynput import mouse, keyboard
+import sys
+import numpy as np
+import cv2
 from PIL import ImageGrab
 import time
+from pynput.keyboard import Key,Controller as KeyboardController
+from pynput.mouse import Button,Controller as MouseController
+import threading
+import mouse, keyboard
+import csv
+print(threading.enumerate())
 
+# Keyboard and mouse controller
+keyboard_cont = KeyboardController()
+mouse_cont = MouseController()
 
-class KeyLogger():
-    def __init__(self, filename: str = str(time.strftime('%H-%M-%S'))+"_keylogs.txt") -> None:
-        self.filename = 'logs_text/'+filename
+# Windows size
+window_pos = [10, 30, 1920, 1040]
+n = 0
+count = 1
 
-    def make_snap(self):
-        snapshot = ImageGrab.grab()
-        file_name = str(time.strftime('%H-%M-%S'))+".png"
-        snapshot.save('logs_screens/'+file_name)
+# Main function
+def start_grab(n,count):
+    # Button name
+    name_button = ["w", "a", "s", "d","shift", "ctrl","space", "e", "q",
+                   None,None,None,None,None,None,
+                   "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "esc"]
+    # Markup list
+    key_list = [0] * 27
 
-    def on_press(self, key):
-        print("some press")
-        self.make_snap()
-        with open(self.filename, 'a') as logs:
-            logs.write(str(time.strftime('%H-%M-%S'))+','+str(key)+'\n')
+    # Cursor position
+    x_pos = 960
+    y_pos = 531
+    with open("Markup.csv", mode="a", encoding='utf-8') as w_file:
+        file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+        while True:
+            # Capture Minecraft windows
+            img = ImageGrab.grab(window_pos)
 
-    def on_move(self, x, y):
-        print("some move")
-        with open(self.filename, 'a') as logs:
-            logs.write(str(time.strftime('%H-%M-%S'))+', Pointer moved to '+ 'x: ' + str(x) + ', y:' +str(y) +'\n')
+            # Mouse movement control
+            if mouse_cont.position[0] > x_pos + 1:
+                key_list[10] = 1
+            elif mouse_cont.position[0] < x_pos - 1:
+                key_list[11] = 1
+            if mouse_cont.position[1] > y_pos + 4:
+                key_list[12] = 1
+            elif mouse_cont.position[1] < y_pos - 4:
+                key_list[9] = 1
 
-    def on_click(self, x, y, button, pressed):
-        print("some click")
-        self.make_snap()
-        with open(self.filename, 'a') as logs:
-            logs.write(str(time.strftime('%H-%M-%S'))+', Mouse click '+ 'x: ' + str(x) + ', y:' +str(y) + ', button: '+ str(button) +'\n')
-        if not pressed:
-            # Stop listener
-            return False
+            # Control mouse button
+            if mouse.is_pressed(mouse.LEFT):
+                key_list[13] = 1
+            if mouse.is_pressed(mouse.RIGHT):
+                key_list[14] = 1
 
-    def on_release(self, key):
-        if key == keyboard.Key.esc:
-            # Stop listeners
-            m_listener.stop()
-            k_listener.stop()
-            return False
+            # Control keyboard key
+            for i in range(len(name_button)):
+                if name_button[i] != None:
+                    if keyboard.is_pressed(name_button[i]):
+                        key_list[i] = 1
 
-    def main(self):
-        with mouse.Listener(on_move=self.on_move, on_click=self.on_click) as m_listener, \
-            keyboard.Listener(on_release=self.on_release, on_press=self.on_press,) as k_listener:  
-                m_listener.run()
-                k_listener.run()
+            # Save image every 5 tick
+            if n == 5:
+                # Change the image color to the rgb
+                img = np.array(img)[:, :, ::-1]
+                # Resize image
+                img = cv2.resize(img, (800, 400))
+                # Show the capture window
+                # cv2.imshow(f"screen{n}",img)
+                # Save image
+                path = f"Image\{count}.jpg"
+                cv2.imwrite(path,img)
+                key_list[26] = f"{count}.jpg"
+                print(key_list)
+                file_writer.writerow(key_list)
+                count += 1
+                n = 0
+                key_list = [0] * 27
+            else:
+                n += 1
+            if (cv2.waitKey(1) & 0xFF) == ord('q'):
+                cv2.destroyAllWindows()
 
+start_grab(0,count)
 
-if __name__ == '__main__':
-    logger = KeyLogger()
-    logger.main()
-        
-        
-        
+def show_mouse_pos():
+    while True:
+        print(mouse_cont.positin)
